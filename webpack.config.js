b447cc7062a6dbf2dce2c4cb516b57e9da7fc723
Stefan Trinh <stefan.trinh@ocado.com>
2019-04-16 23:29:45 +0100

    removed scripts folder and moved index.html and app.js to topm level

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, "/public/scripts/"),
+    path: path.join(__dirname, "/public/"),
     filename: "app.js"
   },
   module: {
